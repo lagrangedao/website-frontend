f1f60ff2c74d7a41f7856c19d7601277c82bdfb6
yxie@nbai.io <86989900+hiwuyi@users.noreply.github.com>
2023-03-30 23:51:07 +0800

    Add access tokens (#26)
    * Add file delete
    
    * Add space payment
    
    * Update license
    
    * Update and optimize the dataset/space setting tab
    
    * Fix mobile UI
    
    * Update https
    
    * Update space
    
    * Fix url cross domain
    
    * Delete backup files
    
    * Change fetch url from acl to ipfs
    
    * Update history mode
    
    * Update publicPath
    
    * Add access token
    
    * Update menu
    
    * Fix style

---
+++ src/views/dashboard/personalCenter/editOrganizations.vue
@@ -0,0 +1,254 @@
+<template>
+  <section id="access_token" v-loading="listLoad">
+    <div class="list">
+      <div class="title">Organizations Settings</div>
+    </div>
+
+    <div class="token_list">
+      <div class="card">
+        <div class="card_header">
+          <div class="card_left">
+            <div class="face"></div>
+            <small>Non-Profit</small>
+          </div>
+          <div class="card_right">
+            <el-button size="large" class="token_button" @click="detailSetting">
+              <el-icon>
+                <Setting />
+              </el-icon>
+            </el-button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+<script>
+const ethereum = window.ethereum;
+import { defineComponent, computed, onMounted, onActivated, onDeactivated, watch, ref, reactive, getCurrentInstance } from 'vue'
+import { useStore } from "vuex"
+import { useRouter, useRoute } from 'vue-router'
+import moment from 'moment'
+import {
+  Setting
+} from '@element-plus/icons-vue'
+export default defineComponent({
+  name: 'Personal Center',
+  components: {
+    Setting
+  },
+  setup () {
+    const store = useStore()
+    const metaAddress = computed(() => (store.state.metaAddress))
+    const accessAvatar = computed(() => (store.state.accessAvatar))
+    const navLogin = computed(() => { return String(store.state.navLogin) === 'true' })
+    const lagLogin = computed(() => { return String(store.state.lagLogin) === 'true' })
+    const tokenData = reactive({
+      token: ''
+    })
+    const listLoad = ref(false)
+    const tokenShow = ref(false)
+    const system = getCurrentInstance().appContext.config.globalProperties
+    const route = useRoute()
+    const router = useRouter()
+
+
+    function detailSetting () {
+      router.push({ name: 'organizationsSettings', params: { menu: 'token' } })
+    }
+    onMounted(() => { })
+    onActivated(() => {
+      tokenData.token = ''
+    })
+    return {
+      metaAddress,
+      accessAvatar,
+      navLogin,
+      lagLogin,
+      system,
+      tokenData,
+      listLoad,
+      tokenShow,
+      detailSetting
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+#access_token {
+  position: relative;
+  max-width: 620px;
+  padding: 0.15rem 0 0;
+  color: #333;
+  font-size: 18px;
+  text-align: left;
+  @media screen and (max-width: 1200px) {
+    font-size: 16px;
+  }
+  :deep(.token_list) {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 0 0 0.2rem;
+    .title {
+      display: block;
+      width: 100%;
+      padding: 0.35rem 0 0;
+      font-size: 0.18rem;
+      line-height: 1.5;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.22rem;
+      }
+    }
+    .desc {
+      padding: 0.1rem 0;
+      font-size: 0.16rem;
+      line-height: 1.5;
+      color: #606060;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.2rem;
+      }
+    }
+    .token_button {
+      padding: 0.13rem 0.3rem;
+      margin: 0.15rem 0;
+      background: linear-gradient(180deg, #fefefe, #f0f0f0);
+      border-color: #e1e1e1;
+      border-radius: 0.08rem;
+      font-size: 0.16rem;
+      color: #606060;
+      line-height: 1;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.2rem;
+      }
+      span,
+      svg,
+      path {
+        cursor: pointer;
+      }
+    }
+    .card {
+      width: calc(100% - 0.24rem);
+      padding: 0.12rem;
+      margin: 0.2rem 0;
+      background: linear-gradient(180deg, #fefefe, #f0f0f0);
+      border: 1px solid #e1e1e1;
+      border-radius: 0.05rem;
+      .card_header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 0.18rem;
+        color: #565656;
+        @media screen and (min-width: 1800px) {
+          font-size: 0.22rem;
+        }
+        .face {
+          width: 0.6rem;
+          height: 0.6rem;
+          margin-right: 0.05rem;
+          background-color: #7405ff;
+          border-radius: 0.05rem;
+        }
+        .card_left,
+        .card_right {
+          display: flex;
+          align-items: center;
+          .token_button {
+            padding: 0.12rem;
+            font-size: 0.22rem;
+            cursor: pointer;
+          }
+        }
+        .card_left {
+          align-items: flex-end;
+          color: #606060;
+        }
+      }
+      .card_body {
+        .el-input {
+          border-color: #ececed;
+          .el-input__inner {
+            height: auto;
+            padding: 0.12rem 0.15rem;
+            background-color: #fff !important;
+            border-right: 0;
+            border-color: inherit;
+            line-height: 1;
+          }
+          .el-input-group__append {
+            padding: 0 0.15rem;
+            background-color: #fff;
+            border-left: 0;
+            border-color: inherit;
+          }
+          .action {
+            display: flex;
+            align-items: center;
+            height: 100%;
+          }
+          .icon_copy {
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            background: url(../../../assets/images/icons/icon_36_1.png)
+              no-repeat left center;
+            background-size: auto 100%;
+            cursor: pointer;
+            @media screen and (min-width: 1800px) {
+              width: 18px;
+              height: 18px;
+            }
+            &:hover {
+              opacity: 0.7;
+            }
+          }
+          .icon_look {
+            width: 20px;
+            height: 20px;
+            margin: 0 0.1rem 0 0;
+            background: url(../../../assets/images/icons/icon_42.png) no-repeat
+              left center;
+            background-size: auto 100%;
+            cursor: pointer;
+            @media screen and (min-width: 1800px) {
+              width: 22px;
+              height: 22px;
+            }
+            &:hover {
+              opacity: 0.7;
+            }
+          }
+          .el-input__suffix {
+            right: 0;
+            .el-icon {
+              font-size: 0.2rem;
+              cursor: pointer;
+              svg,
+              path {
+                cursor: pointer;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  :deep(.el-loading-mask) {
+    background-color: rgba(255, 255, 255, 1);
+    .el-loading-spinner {
+      top: 30%;
+    }
+  }
+}
+</style>
+
+
+<i18n>
+{
+  "en": {},
+  "zh": {}
+}
+</i18n>
---
+++ src/views/dashboard/personalCenter/editToken.vue
@@ -0,0 +1,305 @@
+<template>
+  <section id="access_token" v-loading="listLoad">
+    <div class="list">
+      <div class="title">Access Tokens</div>
+    </div>
+
+    <div class="token_list">
+      <div class="title">User Access Tokens</div>
+      <div class="desc">Access tokens programmatically authenticate your identity to the Hugging Face Hub, allowing applications to perform specific actions specified by the scope of permissions (read, write, or admin) granted. Visit the documentation to discover
+        how to use them.</div>
+      <el-button size="large" class="token_button" @click="createToken">New token</el-button>
+
+      <div class="title">Organizations API Tokens </div>
+      <div class="desc">Unlike user access tokens, an Organization API token can only be used to provide read access to repos, or for billing access to a compute resource.</div>
+      <div class="card" v-if="tokenData.token">
+        <div class="card_header">
+          <div class="face"></div>
+          Organization API token
+        </div>
+        <div class="card_body">
+          <el-input v-model="tokenData.token" :type="tokenShow?'text':'password'" readonly placeholder="Access Tokens">
+            <template #append>
+              <div class="action">
+                <i class="icon icon_look" @click="tokenShow=!tokenShow"></i>
+                <i class="icon icon_copy" @click="copyName(tokenData.token)"></i>
+              </div>
+              <!-- <el-icon @click="deleteToken(tokenData.token)">
+                <Delete />
+              </el-icon> -->
+            </template>
+          </el-input>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+<script>
+const ethereum = window.ethereum;
+import { defineComponent, computed, onMounted, onActivated, onDeactivated, watch, ref, reactive, getCurrentInstance } from 'vue'
+import { useStore } from "vuex"
+import { useRouter, useRoute } from 'vue-router'
+import moment from 'moment'
+import {
+  Delete
+} from '@element-plus/icons-vue'
+export default defineComponent({
+  name: 'Personal Center',
+  components: {
+    Delete
+  },
+  setup () {
+    const store = useStore()
+    const metaAddress = computed(() => (store.state.metaAddress))
+    const accessAvatar = computed(() => (store.state.accessAvatar))
+    const navLogin = computed(() => { return String(store.state.navLogin) === 'true' })
+    const lagLogin = computed(() => { return String(store.state.lagLogin) === 'true' })
+    const tokenData = reactive({
+      token: ''
+    })
+    const listLoad = ref(false)
+    const tokenShow = ref(false)
+    const system = getCurrentInstance().appContext.config.globalProperties
+    const route = useRoute()
+    const router = useRouter()
+
+    async function getdataList () {
+      listLoad.value = true
+      tokenData.token = ''
+      const listRes = await system.$commonFun.sendRequest(`${process.env.VUE_APP_BASEAPI}api_token`, 'get')
+      if (listRes && listRes.status === 'success') {
+        if (listRes.data) tokenData.token = listRes.data.token.token
+        else tokenData.token = ''
+      } else {
+        tokenData.token = ''
+        system.$commonFun.messageTip('error', listRes.message ? listRes.message : 'Failed!')
+      }
+      // await system.$commonFun.timeout(500)
+      listLoad.value = false
+    }
+    async function createToken () {
+      listLoad.value = true
+      const listRes = await system.$commonFun.sendRequest(`${process.env.VUE_APP_BASEAPI}api_token`, 'post')
+      if (listRes && listRes.status === 'success') {
+        system.$commonFun.messageTip('success', listRes.message ? listRes.message : 'Success!')
+        getdataList()
+        return
+      } else system.$commonFun.messageTip('error', listRes.message ? listRes.message : 'Failed!')
+      // await system.$commonFun.timeout(500)
+      listLoad.value = false
+    }
+    async function deleteToken (tokenName) {
+      listLoad.value = true
+      let formData = new FormData()
+      formData.append('api_token', tokenName)
+      const listRes = await system.$commonFun.sendRequest(`${process.env.VUE_APP_BASEAPI}api_token`, 'delete', formData)
+      if (listRes && listRes.status === 'success') system.$commonFun.messageTip('success', listRes.message ? listRes.message : 'Delete successfully!')
+      else system.$commonFun.messageTip('error', listRes.message ? listRes.message : 'Delete failed!')
+      getdataList()
+    }
+
+    function copyName (text) {
+      var txtArea = document.createElement('textarea')
+      txtArea.id = 'txt'
+      txtArea.style.position = 'fixed'
+      txtArea.style.top = '0'
+      txtArea.style.left = '0'
+      txtArea.style.opacity = '0'
+      txtArea.value = text
+      document.body.appendChild(txtArea)
+      txtArea.select()
+
+      try {
+        var successful = document.execCommand('copy')
+        var msg = successful ? 'successful' : 'unsuccessful'
+        console.log('Copying text command was ' + msg)
+        if (successful) {
+          system.$commonFun.messageTip('success', 'Copied')
+          return true
+        }
+      } catch (err) {
+        console.log('Oops, unable to copy')
+      } finally {
+        document.body.removeChild(txtArea)
+      }
+      return false
+    }
+    onMounted(() => {
+      getdataList()
+    })
+    onActivated(() => {
+      tokenData.token = ''
+    })
+    return {
+      metaAddress,
+      accessAvatar,
+      navLogin,
+      lagLogin,
+      system,
+      tokenData,
+      listLoad,
+      tokenShow,
+      getdataList, createToken, deleteToken, copyName
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+#access_token {
+  position: relative;
+  max-width: 620px;
+  padding: 0.15rem 0 0.2rem;
+  color: #333;
+  font-size: 18px;
+  text-align: left;
+  @media screen and (max-width: 1200px) {
+    font-size: 16px;
+  }
+  :deep(.token_list) {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 0 0 0.2rem;
+    .title {
+      display: block;
+      width: 100%;
+      padding: 0.35rem 0 0;
+      font-size: 0.18rem;
+      line-height: 1.5;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.22rem;
+      }
+    }
+    .desc {
+      padding: 0.1rem 0;
+      font-size: 0.16rem;
+      line-height: 1.5;
+      color: #606060;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.2rem;
+      }
+    }
+    .token_button {
+      padding: 0.13rem 0.3rem;
+      margin: 0.15rem 0;
+      background: linear-gradient(180deg, #fefefe, #f0f0f0);
+      border-color: #e1e1e1;
+      border-radius: 0.08rem;
+      font-size: 0.16rem;
+      color: #606060;
+      line-height: 1;
+      @media screen and (min-width: 1800px) {
+        font-size: 0.2rem;
+      }
+    }
+    .card {
+      width: calc(100% - 0.5rem);
+      padding: 0.25rem;
+      margin: 0.2rem 0;
+      background-color: #fbfbfc;
+      border: 1px solid #ececec;
+      border-radius: 0.05rem;
+      .card_header {
+        display: flex;
+        align-items: center;
+        margin-bottom: 0.25rem;
+        font-size: 0.18rem;
+        color: #565656;
+        @media screen and (min-width: 1800px) {
+          font-size: 0.22rem;
+        }
+        .face {
+          width: 0.3rem;
+          height: 0.3rem;
+          margin-right: 0.05rem;
+          background-color: #7405ff;
+          border-radius: 0.05rem;
+        }
+      }
+      .card_body {
+        .el-input {
+          border-color: #ececed;
+          .el-input__inner {
+            height: auto;
+            padding: 0.12rem 0.15rem;
+            background-color: #fff !important;
+            border-right: 0;
+            border-color: inherit;
+            line-height: 1;
+          }
+          .el-input-group__append {
+            padding: 0 0.15rem;
+            background-color: #fff;
+            border-left: 0;
+            border-color: inherit;
+          }
+          .action {
+            display: flex;
+            align-items: center;
+            height: 100%;
+          }
+          .icon_copy {
+            display: inline-block;
+            width: 14px;
+            height: 14px;
+            background: url(../../../assets/images/icons/icon_36_1.png)
+              no-repeat left center;
+            background-size: auto 100%;
+            cursor: pointer;
+            @media screen and (min-width: 1800px) {
+              width: 16px;
+              height: 16px;
+            }
+            &:hover {
+              opacity: 0.7;
+            }
+          }
+          .icon_look {
+            width: 18px;
+            height: 18px;
+            margin: 0 0.1rem 0 0;
+            background: url(../../../assets/images/icons/icon_42.png) no-repeat
+              left center;
+            background-size: auto 100%;
+            cursor: pointer;
+            @media screen and (min-width: 1800px) {
+              width: 20px;
+              height: 20px;
+            }
+            &:hover {
+              opacity: 0.7;
+            }
+          }
+          .el-input__suffix {
+            right: 0;
+            .el-icon {
+              font-size: 0.2rem;
+              cursor: pointer;
+              svg,
+              path {
+                cursor: pointer;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  :deep(.el-loading-mask) {
+    background-color: rgba(255, 255, 255, 1);
+    .el-loading-spinner {
+      top: 30%;
+    }
+  }
+}
+</style>
+
+
+<i18n>
+{
+  "en": {},
+  "zh": {}
+}
+</i18n>
