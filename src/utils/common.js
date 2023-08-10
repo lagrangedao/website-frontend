ded8837d55a10fe9f7a8395281843bd23ba2ef37
yxie@nbai.io <yxie@nbai.io>
2023-08-10 11:20:55 +0800

    403 Error Code Logout

--- src/utils/common.js
+++ src/utils/common.js
@@ -32,7 +32,7 @@ async function sendRequest(apilink, type, jsonObject, api_token) {
   } catch (err) {
     console.error(err, err.response)
     messageTip('error', err.response ? err.response.statusText || 'Request failed. Please try again later!' : 'Request failed. Please try again later!')
-    if (err.response && err.response.status === 401) {
+    if (err.response && (err.response.status === 401 || err.response.status === 403)) {
       signOutFun()
     } else if (err.response) {
       // The request has been sent, but the status code of the server response is not within the range of 2xx
