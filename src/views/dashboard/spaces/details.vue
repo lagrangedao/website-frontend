8b8988eec31b2e399636cd31b77c8704c4eb10b4
yxie@nbai.io <yxie@nbai.io>
2023-08-10 17:45:31 +0800

    Fix renew

--- src/views/dashboard/spaces/details.vue
+++ src/views/dashboard/spaces/details.vue
@@ -296,7 +296,7 @@
       </div>
 
       <el-dialog v-model="dialogCont.spaceHardFork" title="" :width="diagWidth" :show-close="true" :close-on-click-modal="false">
-        <space-hardware @handleHard="handleHard" :listdata="allData.space" :renewButton="renewButton"></space-hardware>
+        <space-hardware v-if="dialogCont.spaceHardFork" @handleHard="handleHard" :listdata="allData.space" :renewButton="renewButton"></space-hardware>
       </el-dialog>
       <space-hardware v-if="dialogCont.spaceHardRenew" @handleHard="handleHard" :listdata="allData.space" :renewButton="renewButton"></space-hardware>
   </section>
@@ -348,50 +348,6 @@ export default defineComponent({
     const route = useRoute()
     const router = useRouter()
     const activeName = ref('card')
-    const tableData = ref([
-      {
-        sentence1: '"The cat sat on the mat."',
-        sentence2: '"The cat did not sit on the mat."',
-        idx: '0',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"The cat did not sit on the mat."',
-        sentence2: '"The cat sat on the mat."',
-        idx: '1',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"When you\'ve got no snow,  it\'s really hard to...',
-        sentence2: '"When you\'ve got snow, it\'s really hard to learn a snowy...',
-        idx: '2',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"Out of the box, Ouya doesn\'t support media...',
-        sentence2: '"Out of the box, Ouya supports media apps such as Twitch...',
-        idx: '3',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"Out of the box, Ouya doesn\'t support media...',
-        sentence2: '"Out of the box, Ouya supports media apps such as Twitch...',
-        idx: '4',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"Out of the box, Ouya supports Twitch.tv...',
-        sentence2: '"Out of the box, Ouya supports media apps such as Twitch...',
-        idx: '5',
-        label: '1   (not_entailment)'
-      },
-      {
-        sentence1: '"Out of the box, Ouy supports media apps...',
-        sentence2: '"Out of the box, Ouya supports Twitch.tv and XBMC media player."',
-        idx: '6',
-        label: '1   (not_entailment)'
-      }
-    ])
     const settingOneself = ref(false)
     const forkLoad = ref(false)
     const parentValue = ref('')
@@ -584,7 +540,6 @@ export default defineComponent({
       route,
       router,
       settingOneself,
-      tableData,
       forkLoad,
       nft,
       nftTokens,
