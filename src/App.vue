c47cc2ddc738486a578be07f41ccec5d1d6c4a30
yxie@nbai.io <yxie@nbai.io>
2024-01-17 19:26:59 +0800

    Fix UI

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="landing">
+    <div class="landing" :class="{'landing-app':(route.name === 'spaceDetail' && route.params.tabs === 'app')}">
         <router-view/>
     </div>
 </template>
-<script setup>
+<script>
 import { provide } from "vue"
+import { defineComponent } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+export default defineComponent({
+    setup () {
+        const route = useRoute()
+        const router = useRouter()
+
+        return {
+            route,
+            router
+        }
+    }
+})
 </script>
 <style>
 @import "./assets/css/style.css";
